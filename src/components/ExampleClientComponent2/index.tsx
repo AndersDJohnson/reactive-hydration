3e1a3ef14b5420a851b6d20c5f1e0eeccea57842
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-20 13:38:17 -0500

    wip

--- src/components/ExampleClientComponent2/index.tsx
+++ src/components/ExampleClientComponent2/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import { useAtom } from "jotai";
 import { textState } from "../../state/textState";
 import { text2State } from "../../state/text2State";
@@ -6,12 +7,18 @@ export const ExampleClientComponent2 = () => {
   const [text] = useAtom(textState);
   const [text2] = useAtom(text2State);
 
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => setCount((c) => c + 1), []);
+
   return (
     // This state metadata could perhaps be injected in a wrapper by the compiler...
     <div data-component="ExampleClientComponent2" data-states="text2State">
       <h2>ExampleClientComponent2</h2>
       <div>SERVER? {(typeof window !== "object").toString()}</div>
       <div>TEXT 2 STATE: {text2}</div>
+      <div>COUNT: {count}</div>
+      <button onClick={handleClick}>count++</button>
     </div>
   );
 };
