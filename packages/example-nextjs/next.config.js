669cb9d2c51acb1fc8ed5a8f493f225e66c0deea
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-21 13:51:45 -0500

    fix-next-export-base-path

--- packages/example-nextjs/next.config.js
+++ packages/example-nextjs/next.config.js
@@ -1,23 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // swcMinify: false,
+const { PHASE_EXPORT } = require("next/constants");
 
-  // webpack(config, { isServer }) {
-  //   // config.optimization.minimize = false;
+module.exports = (phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    basePath:
+      phase === PHASE_EXPORT
+        ? "/reactive-hydration/packages/example-nextjs/out"
+        : "",
 
-  //   // if (!isServer) {
-  //   //   config.module.rules.push({
-  //   //     // Note: loader functions should have a `*ServerComponent/loader.ts` path
-  //   //     // to avoid being excluded by this rule.
-  //   //     test: /ServerComponent\/index/,
-  //   //     use: "null-loader",
-  //   //   });
-  //   // }
+    reactStrictMode: true,
 
-  //   return config;
-  // },
+    swcMinify: true,
+    // swcMinify: false,
+
+    // webpack(config, { isServer }) {
+    //   // config.optimization.minimize = false;
+
+    //   // if (!isServer) {
+    //   //   config.module.rules.push({
+    //   //     // Note: loader functions should have a `*ServerComponent/loader.ts` path
+    //   //     // to avoid being excluded by this rule.
+    //   //     test: /ServerComponent\/index/,
+    //   //     use: "null-loader",
+    //   //   });
+    //   // }
+
+    //   return config;
+    // },
+  };
+
+  return nextConfig;
 };
-
-module.exports = nextConfig;
