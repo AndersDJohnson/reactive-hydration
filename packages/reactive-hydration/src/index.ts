01c848edc1f8dddb1f65d7f6d5f006c50601b2f0
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-24 18:14:09 -0500

    fix-export-ContextWithDefaultValues

--- packages/reactive-hydration/src/index.ts
+++ packages/reactive-hydration/src/index.ts
@@ -2,5 +2,6 @@
 export { ContextSerialized } from "./ContextSerialized";
 export { reactiveHydrate } from "./ReactiveHydrate";
 export { createContextWithDefaultValue } from "./createContextWithDefaultValue";
+export type { ContextWithDefaultValues } from "./createContextWithDefaultValue";
 export { registerState } from "./stateRegistry";
 export { ReactiveHydrationContainer } from "./ReactiveHydrationContainer";
