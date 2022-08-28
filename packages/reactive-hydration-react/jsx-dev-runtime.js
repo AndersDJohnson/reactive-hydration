6749ff78b54d26b90c6074f3d1c5749ce1d3ceeb
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-28 12:30:21 -0500

    wip

--- packages/reactive-hydration-react/jsx-dev-runtime.js
+++ packages/reactive-hydration-react/jsx-dev-runtime.js
@@ -3,9 +3,12 @@ const jsxDevRuntime = require("_react/jsx-dev-runtime");
 const { jsxDEV: origJsxDEV } = jsxDevRuntime;
 
 const React = require("_react");
-const { useEffect } = require("_react");
 
-const { useState } = React;
+const { useContext } = React;
+
+const {
+  SerializedStateContext,
+} = require("reactive-hydration/dist/SerializedStateContext");
 
 console.log("*** reactive-hydration-react/jsx-dev-runtime _react id", React.id);
 
@@ -13,31 +16,14 @@ const jsxDEV = (type, ...args) => {
   const [props, ...restArgs] = args;
 
   const Wrapper = () => {
-    const [state, setState] = useState(() => Math.random());
+    const serializedStateContext = useContext(SerializedStateContext);
 
     console.log(
-      "*** reactive-hydration-react/jsx-dev-runtime Wrapper state",
-      state
+      "*** reactive-hydration-react/jsx-dev-runtime Wrapper serializedStateContext",
+      serializedStateContext
     );
 
-    useEffect(() => {
-      const i = setInterval(() => {
-        setState(Math.random());
-      }, 1000);
-
-      return () => {
-        clearInterval(i);
-      };
-    }, []);
-
-    return origJsxDEV(
-      type,
-      {
-        ...props,
-        "data-test-useState": state,
-      },
-      ...restArgs
-    );
+    return origJsxDEV(type, props, ...restArgs);
   };
 
   Wrapper.displayName = "Wrapper";
