8a5f5f25e063326247123d9a2811bc889ebc3ba8
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-28 12:23:58 -0500

    working-jsx-dev-runtime-using-useState-monkeypatch

--- packages/reactive-hydration-react/jsx-dev-runtime.js
+++ packages/reactive-hydration-react/jsx-dev-runtime.js
@@ -3,21 +3,41 @@ const jsxDevRuntime = require("_react/jsx-dev-runtime");
 const { jsxDEV: origJsxDEV } = jsxDevRuntime;
 
 const React = require("_react");
+const { useEffect } = require("_react");
 
 const { useState } = React;
 
 console.log("*** reactive-hydration-react/jsx-dev-runtime _react id", React.id);
 
 const jsxDEV = (type, ...args) => {
+  const [props, ...restArgs] = args;
+
   const Wrapper = () => {
-    const [state, setState] = useState(true);
+    const [state, setState] = useState(() => Math.random());
 
     console.log(
       "*** reactive-hydration-react/jsx-dev-runtime Wrapper state",
       state
     );
 
-    return origJsxDEV(type, ...args);
+    useEffect(() => {
+      const i = setInterval(() => {
+        setState(Math.random());
+      }, 1000);
+
+      return () => {
+        clearInterval(i);
+      };
+    }, []);
+
+    return origJsxDEV(
+      type,
+      {
+        ...props,
+        "data-test-useState": state,
+      },
+      ...restArgs
+    );
   };
 
   Wrapper.displayName = "Wrapper";
