fe8a9fb752f1078cd703bd5dcc329cdd4f51154d
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-08-28 12:57:42 -0500

    i-dont-know

--- packages/reactive-hydration-react/jsx-dev-runtime.js
+++ packages/reactive-hydration-react/jsx-dev-runtime.js
@@ -4,26 +4,75 @@ const { jsxDEV: origJsxDEV } = jsxDevRuntime;
 
 const React = require("_react");
 
-const { useContext } = React;
+// const { useContext } = React;
 
-const {
-  SerializedStateContext,
-} = require("reactive-hydration/dist/SerializedStateContext");
+// const {
+//   SerializedStateContext,
+// } = require("reactive-hydration/dist/SerializedStateContext");
+
+const { reactiveHydrate } = require("reactive-hydration/dist/ReactiveHydrate");
+// const {
+//   reactiveHydrate,
+//   // ReactiveHydrationContainerContext,
+//   // ReactiveHydrationInnardsContext,
+// } =
+//   // global.ReactiveHydrationSingleton ??
 
 console.log("*** reactive-hydration-react/jsx-dev-runtime _react id", React.id);
+
+const reactiveHydrateRegistry = new Map();
 
 const jsxDEV = (type, ...args) => {
   const [props, ...restArgs] = args;
 
-  const Wrapper = () => {
-    const serializedStateContext = useContext(SerializedStateContext);
+  // TODO: Handle memo objects?
+  if (typeof type !== "function") {
+    return origJsxDEV(type, ...args);
+  }
 
-    console.log(
-      "*** reactive-hydration-react/jsx-dev-runtime Wrapper serializedStateContext",
-      serializedStateContext
+  const name =
+    type.displayName ??
+    type.name ??
+    type.render?.displayName ??
+    type.render?.name;
+
+  const Type = type;
+
+  const { states } = Type;
+
+  let ReactiveHydrateType = reactiveHydrateRegistry.get(name);
+
+  if (!ReactiveHydrateType) {
+    ReactiveHydrateType = reactiveHydrate(
+      {
+        name,
+        states,
+      },
+      Type
     );
 
-    return origJsxDEV(type, props, ...restArgs);
+    reactiveHydrateRegistry.set(name, ReactiveHydrateType);
+  }
+
+  const Wrapper = () => {
+    return origJsxDEV(ReactiveHydrateType, {
+      children: origJsxDEV(type, ...args),
+    });
+
+    // // Works!
+    // const serializedStateContext = useContext(SerializedStateContext);
+    // console.log(
+    //   "*** reactive-hydration-react/jsx-dev-runtime Wrapper serializedStateContext",
+    //   serializedStateContext
+    // );
+    // return origJsxDEV(SerializedStateContext.Provider, {
+    //   value: {
+    //     serializableState: ["ok"],
+    //   },
+    //   children: origJsxDEV(type, ...args),
+    // });
+    // // Works!
+    // return origJsxDEV(type, ...args);
   };
 
   Wrapper.displayName = "Wrapper";
