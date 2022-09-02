4afdf709878dd7733c728a0fb19343ab3087d04e
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-09-01 23:43:18 -0500

    wip

--- packages/reactive-hydration/src/react-actual/index.ts
+++ packages/reactive-hydration/src/react-actual/index.ts
@@ -7,50 +7,16 @@ import {
 const useStateActualReference = useStateActual;
 const useContextActualReference = useContextActual;
 
-// const React: typeof import("_react") =
-//   typeof window !== "undefined" && window.React
-//     ? window.React
-//     : require("_react");
-
-// console.log(
-//   "*** reactive-hydration react-actual React.id",
-//   // @ts-expect-error Just for debugging
-//   React.id
-// );
-
-// if (typeof window !== "undefined") {
-//   window.React = window.React || React;
-// }
-
-// const {
-//   useState: useStateActual,
-//   useContext: useContextActual,
-//   // useCallback,
-//   // useRef,
-// } = React;
-
-export const useState = <S>(init: S | (() => S)) => {
-  console.log("*** useStateActual");
-
-  return useStateActualReference(
+export const useState = <S>(init: S | (() => S)) =>
+  useStateActualReference(
     init,
     // @ts-expect-error The bypass argument is checked in our `jsx-runtime` monkeypatches.
     true
   );
-};
 
-export const useContext = <T>(context: Context<T>) => {
-  console.log("*** useContextActual");
-
-  return useContextActualReference(
+export const useContext = <T>(context: Context<T>) =>
+  useContextActualReference(
     context,
     // @ts-expect-error The bypass argument is checked in our `jsx-runtime` monkeypatches.
     true
   );
-};
-
-// For a time we were considering only importing from `react-actual` in this whole module, so other exports were needed. But it seemed to override `useState` wrapper above.
-// export { useCallback, useRef };
-// export default React;
-
-// module.exports = React;
