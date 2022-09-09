f2749a116ad2b21317cfaf6891e3d289aa617375
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-09-09 18:05:28 -0500

    cleanup

--- packages/example-vite/src/entry-server.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOMServer from "react-dom/server";
-import { App } from "./App";
-
-export function render() {
-  // TODO: Do we want to consider `renderToNodeStream` or `renderToPipeableStream`?
-  return ReactDOMServer.renderToString(<App />);
-}
